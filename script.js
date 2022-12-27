a27fb229a3189f6d705adb3141299bd01012dfc7
emmittjames <emmittjames1@gmail.com>
2022-12-27 17:22:18 -0500

    Marker displaying information when clicked

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ function initMap() {
     const uvaCoords = { lat: 38.0336, lng: -78.5080 }
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
-        center: uvaCoords,
+        center: uvaCoords
     })
-    const marker = new google.maps.Marker({
-        position: uvaCoords,
-        map: map,
+
+    var infoWindow = new google.maps.InfoWindow()
+
+    const newCabCoords = { lat: 38.0325, lng: -78.5051 }
+    const newCabMarker = new google.maps.Marker({
+        position: newCabCoords,
+        map: map
     })
+
+    newCabMarker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent("testing")
+        infoWindow.open(newCabMarker.getMap(), newCabMarker)
+    })
+
 }
 
 window.initMap = initMap;
