1d6f1ae6fa49d5a9549fcf1f62a3d3e47597bbf7
emmittjames <emmittjames1@gmail.com>
2022-12-27 02:25:05 -0500

    Centered at UVA

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
+    const uvaCoords = { lat: 38.0336, lng: -78.5080 }
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
-    });
-    // The marker, positioned at Uluru
+        zoom: 15,
+        center: uvaCoords,
+    })
     const marker = new google.maps.Marker({
-        position: uluru,
+        position: uvaCoords,
         map: map,
-    });
+    })
 }
 
 window.initMap = initMap;
