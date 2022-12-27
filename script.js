99d2b556539f8fa9cb67339b9f3ad467bf19ac6a
emmittjames <emmittjames1@gmail.com>
2022-12-27 18:41:16 -0500

    Added clem

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 // Initialize and add the map
 function initMap() {
-    const uvaCoords = { lat: 38.0336, lng: -78.5080 }
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: uvaCoords
-    })
+    const map = createMap()
+    createMarkers(map)
+}
 
+function createMarkers(map){
     var infoWindow = new google.maps.InfoWindow()
-
     const waterFountains = [
         [{ lat: 38.0325, lng: -78.5051 }, "New Cabell Hall"],
-        [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall"]
+        [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall"],
+        [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library"]
     ]
-
     waterFountains.forEach(([coords, name], i) => {
         const marker = new google.maps.Marker({
             position: coords,
             map: map,
             title: name
         })
-
         marker.addListener("click", () => {
             infoWindow.close();
             infoWindow.setContent(
@@ -31,6 +28,29 @@ function initMap() {
             infoWindow.open(map, marker)
         })
     })
+}
+
+function convertRating(rating){
+    return "★"
+}
+
+function createMap(){
+    const uvaCoords = { lat: 38.0336, lng: -78.5080 }
+    var myStyles =[
+        {
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [
+                  { visibility: "off" }
+            ]
+        }
+    ];
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: uvaCoords,
+        styles: myStyles
+    })
+    return map
 }
 
 window.initMap = initMap;
