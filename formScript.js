ef17e9ce95227813b9abf2dd1530e3d98e4f125e
emmittjames <emmittjames1@gmail.com>
2022-12-30 02:51:21 -0500

    Can now see selected temp and flow ratings

--- formScript.js
+++ formScript.js
@@ -1,13 +1,29 @@
 function collectData(){
-    console.log("asdfasdf")
-    let temp = document.getElementById("temp").value
-    console.log(temp)
-
-    let flow = document.getElementById("flow").value
-    console.log(flow)
-
     let fountain = document.getElementById("fountain").value
     console.log(fountain)
 
+    console.log("Temp: " + selectedTempRating())
+    console.log("Flow: " + selectedFlowRating())
+
     alert("Your response has been recorded. Thank you!")    
+}
+
+function selectedTempRating(){
+    const temps = document.getElementsByName("temp")
+    for(let i=0;i<temps.length;i++){
+        if(temps[i].checked){
+            return 5-i
+        }
+    }
+    return -1
+}
+
+function selectedFlowRating(){
+    const temps = document.getElementsByName("flow")
+    for(let i=0;i<temps.length;i++){
+        if(temps[i].checked){
+            return 5-i
+        }
+    }
+    return -1
 }
