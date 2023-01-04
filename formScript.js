3cf2b373fd5429b6558acacbe3fe7118d2499014
emmittjames <emmittjames1@gmail.com>
2023-01-03 21:49:35 -0500

    Submit button now has room

--- formScript.js
+++ formScript.js
@@ -1,10 +1,6 @@
 var buildingSelect = document.getElementById("buildingSelect")
 var fountainSelect = document.getElementById("fountainSelect")
-const waterFountains = [
-    [{ lat: 38.0325, lng: -78.5051 }, "New Cabell Hall", ["1st floor", "2nd floor", "3rd floor", "4th floor", "5th floor"]],
-    [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall", ["1st floor", "2nd floor", "3rd floor", "4th floor", "5th floor"]],
-    [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library", ["1st floor", "2nd floor", "3rd floor", "4th floor"]]
-]
+const waterFountains = waterFountainData
 waterFountains.forEach(([coords, name, fountainLocations]) => {
     var option = document.createElement("option")
     option.value = name
--- waterFountainData.js
+++ waterFountainData.js
@@ -1,5 +1,5 @@
 const waterFountainData = [
-    [{ lat: 38.0325, lng: -78.5051 }, "New Cabell Hall", ["5th floor", "4th floor", "3rd floor", "2nd floor", "1st floor"]],
-    [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall", ["5th floor", "4th floor", "3rd floor", "2nd floor", "1st floor"]],
-    [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library", ["4th floor", "3rd floor", "2nd floor", "1st floor"]]
+    [{ lat: 38.0325, lng: -78.5051 }, "New Cabell Hall", ["1st floor men's bathroom side", "1st floor women's bathroom side", "2nd floor men's bathroom side", "2nd floor women's bathroom side", "3rd floor men's bathroom side", "3rd floor women's bathroom side", "4th floor men's bathroom side", "4th floor women's bathroom side", "5th floor men's bathroom side", "5th floor women's bathroom side"]],
+    [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall", ["1st floor", "2nd floor", "3rd floor", "3th floor", "5th floor"]],
+    [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library", ["1st floor", "2nd floor", "3rd floor", "4th floor"]]
 ]
