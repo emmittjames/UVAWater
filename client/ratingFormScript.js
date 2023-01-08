f3426258b45cffc6e070a74cb85a937b743e55f8
emmittjames <emmittjames1@gmail.com>
2023-01-08 03:29:17 -0500

    MySQL being annoying :/

---
+++ client/ratingFormScript.js
@@ -0,0 +1,76 @@
+const { Axios } = require("axios")
+
+var buildingSelect = document.getElementById("buildingSelect")
+var fountainSelect = document.getElementById("fountainSelect")
+const waterFountains = waterFountainData
+waterFountains.forEach(([coords, name, fountainLocations]) => {
+    var option = document.createElement("option")
+    option.value = name
+    option.text = name
+    buildingSelect.appendChild(option)
+})
+updateWaterFountains()
+
+document.querySelector("#buildingSelect").addEventListener("change",function(){
+    updateWaterFountains()
+})
+
+function updateWaterFountains(){
+    for(let i=fountainSelect.options.length-1;i>=0;i--){
+        fountainSelect.remove(i)
+    }
+    waterFountains.forEach(([coords, name, fountainLocations]) => {
+        if(name === buildingSelect.value){
+            fountainLocations.forEach((location) => {
+                var option = document.createElement("option")
+                option.value = location
+                option.text =  location
+                fountainSelect.appendChild(option)
+            })
+        }
+    })
+}
+
+function collectData(){
+    const building = document.getElementById("buildingSelect").value
+    const fountain = document.getElementById("fountainSelect").value
+    const temp = selectedTempRating()
+    const flow = selectedFlowRating()
+    if(temp<0 || flow<0){
+        return
+    }
+
+    console.log(building)
+    console.log(fountain)
+    console.log("Temp: " + temp)
+    console.log("Flow: " + flow)
+    
+    Axios.post("http://localhost:3000/create", {
+        building: building, 
+        ountain: fountain, 
+        temp: temp, 
+        flow: flow
+    })
+
+    alert("Your response has been recorded. Thank you!")    
+}
+
+function selectedTempRating(){
+    const temps = document.getElementsByName("temp")
+    for(let i=0;i<temps.length;i++){
+        if(temps[i].checked){
+            return 5-i
+        }
+    }
+    return -1
+}
+
+function selectedFlowRating(){
+    const temps = document.getElementsByName("flow")
+    for(let i=0;i<temps.length;i++){
+        if(temps[i].checked){
+            return 5-i
+        }
+    }
+    return -1
+}
---
+++ server/sqltest.js
@@ -0,0 +1,48 @@
+const mysql = require("mysql2")
+const express = require("express")
+const axios = require("axios")
+const app = express()
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "2003Esj()",
+    database: "UVAWater"
+  });
+  
+con.connect(function(err) {
+    if (err) throw err
+    console.log("Connected!")
+})
+
+app.post("/create", (req, res) => {
+    const building = req.body.building
+    const fountain = req.body.fountain
+    const temp = req.body.temp
+    const flow = req.body.flow
+
+    let sql = "INSERT INTO reviews (buildingName, fountainName, flowRating, tempRating) VALUES (?,?,?,?)"
+    con.query(sql, [building, fountain, temp, flow], function (err, result) {
+        if (err) throw err
+        console.log(result)
+        res.send("Insterted rating")
+    })
+})
+
+app.get("/createdb", (req, res) => {
+    let sql = "CREATE TABLE IF NOT EXISTS reviews (buildingName VARCHAR(255), fountainName VARCHAR(255), flowRating INT, tempRating INT)"
+    con.query(sql, function (err, result) {
+        if (err) throw err
+        console.log("Table created");
+    })
+    sql = "INSERT INTO reviews (buildingName, fountainName, flowRating, tempRating) VALUES ('Rice Hall', '2nd floor', 4, 2)"
+    con.query(sql, function (err, result) {
+        if (err) throw err
+        console.log(result)
+        res.send("Insterted rating")
+    })
+})
+
+app.listen("3000", () => {
+    console.log("Listening on port 3000")
+})
