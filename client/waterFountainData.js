9887c313fc7f957a3f8bcd65b718a388fe926f79
emmittjames <emmittjames1@gmail.com>
2023-01-12 16:41:18 -0500

    Moved package.json into a folder named app

--- client/waterFountainData.js
+++ client/waterFountainData.js
@@ -1,6 +1,7 @@
 const waterFountainData = [
     [{ lat: 38.0325, lng: -78.5051 }, "New Cabell Hall", ["1st floor men's bathroom side", "1st floor women's bathroom side", "2nd floor men's bathroom side", "2nd floor women's bathroom side", "3rd floor men's bathroom side", "3rd floor women's bathroom side", "4th floor men's bathroom side", "4th floor women's bathroom side", "5th floor men's bathroom side", "5th floor women's bathroom side"]],
-    [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall", ["1st floor", "2nd floor", "3rd floor", "3th floor", "5th floor"]],
+    [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall", ["Basement", "1st floor", "2nd floor", "3rd floor", "4th floor", "5th floor"]],
+    [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library", ["1st floor", "2nd floor", "3rd floor", "4th floor"]]
     [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library", ["1st floor", "2nd floor", "3rd floor", "4th floor"]]
 ]
 const BACKEND_URL = "https://uvawater.up.railway.app"
---
+++ server/index.js
@@ -0,0 +1,44 @@
+const mysql = require("mysql2")
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config({ path: "./.env" })
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const con = mysql.createConnection(process.env.DATABASE_URL)
+const BACKEND_URL = ""
+  
+con.connect(function(err){
+    if (err) throw err
+    console.log("Connected!")
+})
+
+app.post(BACKEND_URL + "/reviews", (req, res) => {
+    const building = req.body.building
+    const sql = "SELECT * FROM reviews WHERE buildingName = ?"
+    con.query(sql, [building], (err, result) => {
+        if(err) throw err
+        res.send(result)
+    })
+})
+
+app.post(BACKEND_URL + "/create", (req, res) => {
+    const building = req.body.building
+    const fountain = req.body.fountain
+    const temp = req.body.temp
+    const flow = req.body.flow
+
+    sql = "INSERT INTO reviews (buildingName, fountainName, flowRating, tempRating) VALUES (?,?,?,?)"
+    con.query(sql, [building, fountain, temp, flow], (err, result) => {
+        if(err) throw err
+        console.log(result)
+        res.send("Insterted rating")
+    })
+})
+
+const PORT = process.env.PORT || "3000"
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT)
+})
