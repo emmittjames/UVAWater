e27f9c55c6302278beb2199f790a2eee0c88c26a
emmittjames <emmittjames1@gmail.com>
2023-01-13 03:49:33 -0500

    Added small hall

--- client/waterFountainData.js
+++ client/waterFountainData.js
@@ -2,11 +2,12 @@ const waterFountainData = [
     [{ lat: 38.0325, lng: -78.5051 }, "New Cabell Hall", ["1st floor men's bathroom side", "1st floor women's bathroom side", "2nd floor men's bathroom side", "2nd floor women's bathroom side", "3rd floor men's bathroom side", "3rd floor women's bathroom side", "4th floor men's bathroom side", "4th floor women's bathroom side", "5th floor men's bathroom side", "5th floor women's bathroom side"]],
     [{ lat: 38.0316, lng: -78.5108 }, "Rice Hall", ["Basement", "1st floor", "2nd floor", "3rd floor", "4th floor", "5th floor"]],
     [{ lat: 38.0364, lng: -78.5061 }, "Clemons Library", ["1st floor", "2nd floor", "3rd floor", "4th floor"]],
-    [{ lat: 38.0321, lng: -78.5107 }, "Olsson Hall", ["Basement", "1st floor"]],
+    [{ lat: 38.0320, lng: -78.5106 }, "Olsson Hall", ["Basement", "1st floor"]],
     [{ lat: 38.0356, lng: -78.5034 }, "The Rotunda", ["Outside water fountain", "Tunnel water fountain"]],
     [{ lat: 38.0331, lng: -78.5078 }, "Clark hall", ["Entrance left side", "Entrance right side"]],
     [{ lat: 38.0358, lng: -78.5067 }, "Newcomb Hall", ["Basement", "Pav", "1st floor hallway", "2nd floor", "3rd floor"]],
     [{ lat: 38.03325, lng: -78.5097 }, "Thornton Hall", ["A-Wing 1st floor", "A-Wing 2nd floor"]],
+    [{ lat: 38.0321, lng: -78.5113 }, "Small Hall", ["1st floor"]],
 ]
 //const BACKEND_URL = "https://uvawater.up.railway.app"
 const BACKEND_URL = "https://breakable-bell-bottoms-moth.cyclic.app"
