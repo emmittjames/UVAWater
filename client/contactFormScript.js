1164095d49398dedc32bfe28ff11415ff8076a8c
emmittjames <emmittjames1@gmail.com>
2023-01-15 21:12:09 -0500

    Added hamburger to contact form

--- client/contactFormScript.js
+++ client/contactFormScript.js
@@ -2,3 +2,9 @@ function sendEmail(){
     console.log("email sent")
     alert("Thank you for your feedback")
 }
+
+const hamburgerToggle = document.getElementsByClassName("hamburgerToggle")[0]
+const navbarLinks = document.getElementsByClassName("navbarLinks")[0]
+hamburgerToggle.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active")
+})
